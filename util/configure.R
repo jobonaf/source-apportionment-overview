5f42c55c500a1a94e1005fcec1e13ce65262fb6e
Giovanni Bonafè <midable@gmail.com>
2021-10-21 13:27:08 +0200

    utilities

---
+++ util/configure.R
@@ -0,0 +1,7 @@
+.libPaths(new = "/u/arpa/bonafeg/R/x86_64-pc-linux-gnu-library/3.5") ## change this as you need
+#Sys.setenv(LD_LIBRARY_PATH=paste("/lib:/usr/lib:/usr/local/lib",Sys.getenv("LD_LIBRARY_PATH"),sep=":"))
+#Sys.setenv(PATH=paste("/opt/software/linux-centos7-x86_64/gcc/8.2.0/proj/5.2.0-dykepst6fje7cih4zmdi7n4infjvvtoh/share/proj:/usr/local/bin/proj/share/proj:/usr/local/bin/proj/bin:/usr/local/bin/proj/lib:/usr/local/bin/proj/lib/pkgconfig:/usr/local/bin/proj/include",Sys.getenv("PATH"),sep=":"))
+#Sys.setenv(PROJ_LIB=c("/opt/software/linux-centos7-x86_64/gcc/8.2.0/proj/5.2.0-dykepst6fje7cih4zmdi7n4infjvvtoh/share/proj"))
+#dyn.load('/usr/local/bin/libproj.so.13')
+#library(rgdal)
+
